d963932b11d0edc16770613872a96b33a6889e21
taskevich <93867297+taskevich@users.noreply.github.com>
2022-05-06 05:08:15 +0300

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,287 @@
+﻿using System;
+
+namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.authorsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Location = new System.Drawing.Point(12, 44);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(600, 600);
+            this.panel1.TabIndex = 0;
+            this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(26, 2);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Количество точек";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(15, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(120, 20);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(15, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(120, 40);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Посчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(141, 18);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(171, 3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Ответ";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(142, 43);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(99, 41);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Очистить поле";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(14, 55);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(150, 17);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "Использовать Threading";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(14, 32);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(131, 17);
+            this.checkBox2.TabIndex = 8;
+            this.checkBox2.Text = "Использовать Tasks";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.checkBox2);
+            this.panel2.Controls.Add(this.checkBox1);
+            this.panel2.Location = new System.Drawing.Point(619, 44);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(252, 100);
+            this.panel2.TabIndex = 9;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(0, 13);
+            this.label3.TabIndex = 9;
+            // 
+            // panel3
+            // 
+            this.panel3.Controls.Add(this.label1);
+            this.panel3.Controls.Add(this.textBox1);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.label2);
+            this.panel3.Controls.Add(this.textBox2);
+            this.panel3.Location = new System.Drawing.Point(618, 150);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(253, 100);
+            this.panel3.TabIndex = 10;
+            // 
+            // panel4
+            // 
+            this.panel4.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.panel4.Controls.Add(this.textBox3);
+            this.panel4.Location = new System.Drawing.Point(619, 256);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(252, 353);
+            this.panel4.TabIndex = 11;
+            this.panel4.Paint += new System.Windows.Forms.PaintEventHandler(this.panel4_Paint);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(-2, -5);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textBox3.Size = new System.Drawing.Size(252, 356);
+            this.textBox3.TabIndex = 0;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(619, 616);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(252, 29);
+            this.button3.TabIndex = 12;
+            this.button3.Text = "Получить статистику";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(883, 24);
+            this.menuStrip1.TabIndex = 13;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.authorsToolStripMenuItem});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // authorsToolStripMenuItem
+            // 
+            this.authorsToolStripMenuItem.Name = "authorsToolStripMenuItem";
+            this.authorsToolStripMenuItem.Size = new System.Drawing.Size(116, 22);
+            this.authorsToolStripMenuItem.Text = "Authors";
+            this.authorsToolStripMenuItem.Click += new System.EventHandler(this.authorsToolStripMenuItem_Click);
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(883, 669);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.panel4);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Monte_Carlo";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
+            this.panel4.ResumeLayout(false);
+            this.panel4.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem authorsToolStripMenuItem;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,269 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Diagnostics;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void monte_carlo(int num)
+        {
+            Random r = new Random();
+            float cnt = 0;
+            for (int i = 0; i < num; i++)
+            {
+                float dx = (float)r.Next(0, 100) / 100.0f;
+                float dy = (float)r.Next(0, 100) / 100.0f;
+
+                if (dx * dx + dy * dy < 1)
+                    ++cnt;
+            }
+
+
+            float pi = (4.0f * ((float)cnt / (float)num));
+
+            if (checkBox1.Checked || checkBox2.Checked)
+                this.textBox1.BeginInvoke((MethodInvoker)delegate
+                {
+                    textBox2.Text = pi.ToString(CultureInfo.InvariantCulture);
+                });
+            else if (!checkBox1.Checked && !checkBox2.Checked)
+                textBox2.Text = pi.ToString(CultureInfo.InvariantCulture);
+        }
+
+        void draw()
+        {
+            SolidBrush blueBrush = new SolidBrush(Color.Black);
+            Pen pen = new Pen(Color.Black, 2);
+            Graphics gPanel = panel1.CreateGraphics();
+
+
+            Point p1 = new Point(panel1.Width / 2, 0);
+            Point p2 = new Point(panel1.Width / 2, panel1.Height);
+            gPanel.DrawLine(pen, p1, p2);
+
+            Point p3 = new Point(0, panel1.Height / 2);
+            Point p4 = new Point(panel1.Width, panel1.Height / 2);
+            gPanel.DrawLine(pen, p3, p4);
+
+            gPanel.DrawEllipse(pen, panel1.Height / 25, panel1.Width / 25, 550, 550);
+        }
+
+        private void log_file(string line)
+        {
+            var fstream = new FileStream("log.txt", FileMode.Append);
+            try
+            {
+                string formatted = line;
+                byte[] buffer = Encoding.Default.GetBytes(formatted);
+                fstream.Write(buffer, 0, buffer.Length);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                fstream.Close();
+            }
+        }
+
+        private void get_stats()
+        {
+            try
+            {
+                //var fstream = new FileStream("log.txt", FileMode.Open);
+                //byte[] buffer = new byte[fstream.Length];
+                //fstream.Read(buffer, 0, buffer.Length);
+                //string stat = Encoding.Default.GetString(buffer);
+                //textBox3.Text = stat;
+                //fstream.Close();
+                var stat = (File.ReadLines("log.txt")).ToList();
+                //textBox3.Text += stat[0] + "\n";
+                foreach (var line in stat)
+                    textBox3.Text += line + "\r" + "\n";
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+            }
+        }
+        private void cls()
+        {
+            Graphics gPanel = panel1.CreateGraphics();
+            gPanel.Clear(Color.White);
+            draw();
+        }
+
+        private void panel1_Paint(object sender, PaintEventArgs e)
+        {
+            draw();
+        }
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Pen pen = new Pen(Color.Black, 2);
+            SolidBrush blueBrush = new SolidBrush(Color.Black);
+            Graphics gPanel = panel1.CreateGraphics();
+            GraphicsState transState = gPanel.Save();
+            Random r = new Random();
+            Stopwatch sw = Stopwatch.StartNew();
+
+            string threading_time = "";
+            string tasks_time = "";
+            string default_time = "";
+
+            int num = 0;
+
+
+            if (!int.TryParse(textBox1.Text, out int n))
+            {
+                MessageBox.Show("Нужно ввести целочисленное число!");
+            }
+            else
+            {
+                try
+                {
+                    num = int.Parse(textBox1.Text);
+
+                    if (checkBox1.Checked && !checkBox2.Checked)
+                    {
+                        sw.Start();
+                        Thread t = new Thread(() => monte_carlo(num));
+                        t.Start();
+                        cls();
+                    }
+                    else if (checkBox2.Checked && !checkBox1.Checked)
+                    {
+                        sw.Start();
+                        Task t = new Task(() => monte_carlo(num));
+                        t.Start();
+                        cls();
+                    }
+                    else if (!checkBox1.Checked && !checkBox2.Checked)
+                    {
+                        sw.Start();
+                        monte_carlo(num);
+                        cls();
+                    }
+                    else if (checkBox1.Checked && checkBox2.Checked)
+                    {
+                        MessageBox.Show("Нельзя использовать задачу и поток одновременно");
+                        num = 0;
+                        cls();
+                    }
+
+                    for (int i = 0; i < num; ++i)
+                    {
+                        int rnd = r.Next(100, 500);
+
+
+                        int x1 = r.Next(600);
+                        int x2 = r.Next(600);
+
+                        Rectangle rect = new Rectangle(x2, x1, 5, 5);
+                        gPanel.FillRectangle(blueBrush, rect);
+                    }
+                    sw.Stop();
+                    TimeSpan ts = sw.Elapsed;
+                    if (checkBox1.Checked)
+                    {
+                        threading_time = "Threading Time " + String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10) + "\n";
+                        log_file(threading_time);
+                    }
+                    else if (checkBox2.Checked)
+                    {
+                        tasks_time = "Tasks Time " + String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10) + "\n";
+                        log_file(tasks_time);
+                    }
+                    else 
+                    {
+                        default_time = "Default Time " + String.Format("{0:00}:{1:00}:{2:00}.{3:00}", ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds / 10) + "\n";
+                        log_file(default_time);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Нужно обязательно ввести количество точек");
+                }
+            }
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            cls();
+        }
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void panel4_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            get_stats();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void authorsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            string caption = "Information";
+            string message = "Программист - Однобурцев\t\n" +
+                             "Документатор - Горбунов\t\t";
+            MessageBoxButtons button = MessageBoxButtons.OK;
+            DialogResult result;
+
+            result = MessageBox.Show(this, message, caption, button,
+            MessageBoxIcon.None, MessageBoxDefaultButton.Button1,
+            MessageBoxOptions.RightAlign);
+            if (result == DialogResult.Yes)
+            {
+                this.Close();
+            }
+        }
+    }
+}
